005685db6c00d7620f402a1350a2dd353c2404e8
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-07-14 15:14:07 -0500

    Problematic exercise

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -6,19 +6,23 @@ namespace ProblematicProblem
 {
     class Program
     {
-        static Random rng = new Random(); 
+        static Random rng = new Random();
 
         static List<string> activities = new List<string>()
         {
             "Movies", "Paintball", "Bowling", "Laser Tag",
-            "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting"
+            "Swimming", "Hiking", "Axe Throwing", "Rock Climbing"
         };
 
         static void Main(string[] args)
         {
             Console.Write("Hello, welcome to the random activity generator!\nWould you like to generate a random activity? (yes/no): ");
             bool cont = Console.ReadLine().ToLower() == "yes";
-
+            if (cont == false)
+            {
+                return;
+            }
+            
             Console.WriteLine();
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
@@ -60,7 +64,7 @@ namespace ProblematicProblem
 
             while (cont)
             {
-                Console.Write("Connecting to the database");
+                /*Console.Write("Connecting to the database");
                 for (int i = 0; i < 10; i++)
                 {
                     Console.Write(". ");
@@ -74,12 +78,12 @@ namespace ProblematicProblem
                     Console.Write(". ");
                     Thread.Sleep(500);
                 }
-                Console.WriteLine();
+                */ Console.WriteLine();
 
                 int randomNumber = rng.Next(activities.Count);
                 string randomActivity = activities[randomNumber];
 
-                if (userAge > 21 && randomActivity == "Wine Tasting")
+                if (userAge < 21 && randomActivity == "Rock Climbing")
                 {
                     Console.WriteLine($"Oh no! Looks like you are too young to do {randomActivity}");
                     Console.WriteLine("Pick something else!");
@@ -95,3 +99,4 @@ namespace ProblematicProblem
         }
     }
 }
+
